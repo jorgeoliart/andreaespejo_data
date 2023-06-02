296748032a7869914f07637634c48c9b87c2459d
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2023-06-02 17:19:00 -0500

    Generar muestra de 250 observaciones/filas

--- andreaespejo.R
+++ andreaespejo.R
@@ -5,3 +5,9 @@ faculty <- read.csv("faculty.csv") #importar base de datos
 
 set.seed(280851373) #fijar set.seed para fijar la aleatorización de la muestra
 
+muestra_n250 <- dplyr::slice_sample( #generar muestra de 250 filas
+  faculty,
+  n = 250,
+  replace = TRUE #definir que sea posible repetir una misma observación en el muestreo
+)
+
