0dffe3960124cd22b72d5cd707cd2d4b01954515
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2023-06-02 17:15:00 -0500

    Cargar librería dplyr, para agrupar datos y resumir estadísticos

--- andreaespejo.R
+++ andreaespejo.R
@@ -1,3 +1,7 @@
+#cargar las librería a ser usadas
+library(dplyr) #cargar librería que permite agrupar data y resumir estadísticos
+
 faculty <- read.csv("faculty.csv") #importar base de datos
 
 set.seed(280851373) #fijar set.seed para fijar la aleatorización de la muestra
+
