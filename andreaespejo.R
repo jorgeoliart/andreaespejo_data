e3e9923785408ee98544a7810fa96cc877d4ccc4
jorgeoliart <jorgeoliart.gmail.com>
2023-06-03 18:33:44 -0500

    Chequear el supuesto de normalidad de la distribución de salarios en h y m

--- andreaespejo.R
+++ andreaespejo.R
@@ -18,9 +18,14 @@ muestra_stats <- muestra_n250 %>% #obtener estadísticos de tendencia central y 
             desvest = sd(salary),
             rango =  max(salary) - min(salary))
 
-#generar submuestras de hombres y mujeres, previo al test t
+#chequear los supuestos previos al uso del test t
+#generar submuestras de hombres y mujeres, previo al test t de welch
 hombres <- muestra_n250[muestra_n250$male == 1, "salary"]
 mujeres <- muestra_n250[muestra_n250$male == 0, "salary"]
+
+#chequear el supuesto de normalidad de la distribución de salarios
+shapiro.test(hombres)
+shapiro.test(mujeres)
 
 t_result <- t.test (hombres, mujeres, var.equal = FALSE) #Calcular t de welch para salarios entre hombres y mujeres
 
